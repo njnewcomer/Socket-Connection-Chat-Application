4c4816d735cec4e3401f96bb2ee1df54c834c6ec
njnewcomer <njnewcomer@wisc.edu>
2025-09-14 19:09:10 -0500

    Threading in Client Updated

--- ClientApplication.py
+++ ClientApplication.py
@@ -1,10 +1,11 @@
 import socket
 import threading
 import tkinter
+import time
 
 # First, get the server's ip address, then use an unused port
 # Think of the Host as the street address, with the internet being the city
-HOST = '10.141.215.186'
+HOST = '10.141.64.144'
 
 # Specifies the endpoint where communication occurs
 # Think of the Port as the house number
@@ -20,31 +21,25 @@ STYLE = "utf-8"
 # Indicates whether a single client is connected to the server or not
 connected = False
 
-
+# Connection for the client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-""" with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-    # Attempt to establish connection with the server
-    connection = client.connect((HOST, PORT))
-    print(f"Connection successful at ", {connection})
-    # Send a message to the server
-    message = "S"
-    client.send(message.encode(STYLE))
-    # Receive data from the server
-    data = client.recv(HEADER)
-    # Print out the data received in utf-8 formatting
-    message = data.decode(STYLE )
-    print(f"Server: {message}")"""
+def get_input():
+    global connected
+    time.sleep(1)
+    while connected:
+        # This will block
+        input_message = input("Client: ")
+        send_message(input_message)
 
 def send_message(message: str):
     global client
-    # Send a message to the server
-    client.send(message.encode(STYLE))
-    # Receive data from the server
-    data = client.recv(HEADER)
-    # Print out the data received in utf-8 formatting
-    message = data.decode(STYLE)
-    print(f"Server: {message}") 
+    try:
+        # Send a message to the server
+        client.send(message.encode(STYLE))
+    except OSError as e:
+        print(f"Failed to send: ", {e})
+
 
 def listen_for_messages():
     global connected
@@ -54,20 +49,28 @@ def listen_for_messages():
         print(f"Server: {message}")
 
 
+
+
 def main():
     global client, HOST, PORT, connected
     print("Welcome to the Server-Client Chat Application!\n")
 
     # Attempt to establish connection with the server
+    print("Waiting to connect to server...")
     connection = client.connect((HOST, PORT))
+    print(f"Connected to {HOST}")
     connected = True
 
-    # message = "S"
-    # send_message(message)
-
+    # Create and start a separate thread that reads messages from the server
     listening_thread = threading.Thread(target=listen_for_messages)
     listening_thread.start()
 
+    connection_thread = threading.Thread(target=get_input)
+    connection_thread.start()
+
+    while connected:
+        pass
+
+
 if __name__ == "__main__":
     main()
-
